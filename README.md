0bd917cc35a4b551d6e81c43ee1b4c89b469b1bb
aikawa294224612 <yellowgirl3614@gmail.com>
2019-03-17 20:12:44 +0800

    20190317

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Dcard_Crawler
+# Dcard 爬蟲-熱門發文時間統計
+
+### request
+
+    request({
+        url: "你想抓的網址",
+        method: "GET"
+      }, function(e,r,b) { /* Callback 函式 */
+        /* e: 錯誤代碼 */
+        /* b: 傳回的資料內容 */
+      });
+### Cheerio
+
+     $ = cheerio.load(blogHTMLString);
+      titles = $("li.item h2");
+	 var result = [];
+	  for(i=0;i<titles.length;i++) { result.push($(titles[i]).text()); }
+
+
+### node-schedule
+[ Nodejs定时任务（node-schedule)](https://www.jianshu.com/p/8d303ff8fdeb)
+
+    npm install node-schedule --save
+
+**Cron风格定时器**
+
+    const schedule = require('node-schedule');
+    
+    const  scheduleCronstyle = ()=>{
+      //每分钟的第30秒定时执行一次:
+        schedule.scheduleJob('30 * * * * *',()=>{
+            console.log('scheduleCronstyle:' + new Date());
+        }); 
+    }
+    
+    scheduleCronstyle();
+
+参数
+
+    每分钟的第30秒触发： '30 * * * * *'
+    
+    每小时的1分30秒触发 ：'30 1 * * * *'
+    
+    每天的凌晨1点1分30秒触发 ：'30 1 1 * * *'
+    
+    每月的1日1点1分30秒触发 ：'30 1 1 1 * *'
+    
+    2016年的1月1日1点1分30秒触发 ：'30 1 1 1 2016 *'
+    
+    每周1的1点1分30秒触发 ：'30 1 1 * * 1'
+
+
+[資料視覺化D3.js的範例](https://blog.twtnn.com/2015/06/d3js.html)
+
+[Simple Line Graph using SVG and d3.js](http://bl.ocks.org/benjchristensen/2579599)
+
+[資料爬蟲實戰－使用 node.js](http://blog.infographics.tw/2015/03/crawl-data-with-nodejs/)
---
+++ routes/index.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var request = require("request");
+var cheerio = require("cheerio");
+var mysql = require('mysql');
+var schedule = require('node-schedule');
+var info = require('../public/javascripts/test.js');
+
+var router = express.Router();
+
+var dates_arr = [];
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : info.pw,
+  database : info.dbase,
+});
+connection.connect();
+
+function databasecheck(hour) {
+  return new Promise((resolve, reject) => {
+     //select
+	    	var  sql = 'SELECT time FROM dcard_time';
+			connection.query(sql,function (err, result) {
+				for (var i = 0; i < result.length; i++) {
+					
+				if(hour==result[i].time){
+					var has=1;
+					break;
+					}else{
+						var has=0;
+					}							
+				}
+				if(has==1){
+				var modSql = 'UPDATE dcard_time SET count = count+1 WHERE time=?';
+				var modSqlParams = [hour];
+
+				connection.query(modSql,modSqlParams,function (err, result) {
+				   if(err){
+				         console.log('[UPDATE ERROR] - ',err.message);
+				         return;
+				   }        
+				  console.log('updated!');				  
+				});
+				
+
+				}else{
+				var  addSql = 'INSERT INTO dcard_time(time,count) VALUES(?,?)';
+			        var  addSqlParams = [hour,1];
+
+					connection.query(addSql,addSqlParams,function (err, result) {
+		        if(err){
+		         console.log('[INSERT ERROR] - ',err.message);
+		         return;
+		        }        
+		 			console.log("inserted!");  
+		}); 
+					
+				}
+			
+		
+});
+  });
+}
+
+var scheduleCronstyle = ()=>{
+    schedule.scheduleJob('30 1 1 * * *',()=>{
+       request({
+    url: "https://www.dcard.tw/f",
+    method: "GET"
+  }, function(err,res,body) {
+    if(!err){
+    	// console.log(body);
+    	var $ = cheerio.load(body);
+		var update_time = $(info.class);
+
+		// console.log(update_time);
+	    for(var i=0;i<update_time.length;i++) {
+	      dates_arr.push(($(update_time[i]).text().toString().split(" "))[1]); 
+	      // console.log((dates_arr[i].toString().split(" "))[1]);
+	      // console.log((dates_arr[i].toString().split(":"))[0]);  
+	     databasecheck((dates_arr[i].toString().split(":"))[0]);
+	    }
+	   
+		
+    }
+    	
+  });
+ 
+
+    }); 
+}
+
+scheduleCronstyle();
+
+  
+ 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+var send=[];
+	var  sql = 'SELECT * FROM dcard_time ORDER BY time';
+			connection.query(sql,function (err, result) {
+				for (var i = 0; i < result.length; i++) {
+					send.push(result[i].count);
+				}
+				console.log(send);
+				res.render('index', { data_array: send });
+  });
+ 
+});
+// console.log(dates_arr[0]);
+module.exports = router;
